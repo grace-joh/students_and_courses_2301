3b9d9990a7c7111bb1b57bc89b2695ef5d6b98e2
Grace Joh <graceehjoh@gmail.com>
2023-02-15 11:30:37 -0600

    feat: add Gradebook #students_below and refactor #list_all_students

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -11,10 +11,26 @@ class Gradebook
   end
 
   def list_all_students
+    return 'No courses have been added.' if @courses.empty?
+
     all_students = {}
     @courses.each do |course|
       all_students[course.name.to_sym] = course.students
     end
     all_students
+  end
+
+  def students_below(threshold)
+    return 'No courses have been added.' if @courses.empty?
+    return 'No students are currently enrolled.' if self.list_all_students.values.flatten.empty?
+
+    students_below = []
+    list_all_students.each do |_course, students|
+      students.each { |student| students_below << student if student.grade < threshold }
+    end
+
+    return "All student averages are above #{threshold}." if students_below.empty?
+
+    students_below
   end
 end
