89bec76067aa97f9c774f7a7051ebb36c8be3b96
Grace Joh <graceehjoh@gmail.com>
2023-02-15 11:04:24 -0600

    test: add test for Gradebook #list_all_students

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -5,10 +5,13 @@ RSpec.describe Gradebook do
     @gradebook1 = Gradebook.new('John Quintanilla')
     @gradebook2 = Gradebook.new('Dallas Williams')
     @calculus = Course.new('Calculus', 2)
-    @lin_al = Course.new('Linear Algebra', 4)
+    @algebra = Course.new('Algebra', 4)
     @student1 = Student.new({ name: 'Morgan', age: 21 })
     @student2 = Student.new({ name: 'Jordan', age: 29 })
-    @student3 = Student.new({ name: "Grace", age: 31 })
+    @student3 = Student.new({ name: 'Grace', age: 31 })
+    @calculus.enroll(@student1)
+    @calculus.enroll(@student2)
+    @algebra.enroll(@student3)
   end
 
   describe '#initialize' do
@@ -29,8 +32,17 @@ RSpec.describe Gradebook do
     it 'can add a course to instructors gradebook' do
       @gradebook1.add_course(@calculus)
 
-      expect(@gradebook1.add_course(@lin_al)).to eq([@calculus, @lin_al])
-      expect(@gradebook1.courses).to eq([@calculus, @lin_al])
+      expect(@gradebook1.add_course(@algebra)).to eq([@calculus, @algebra])
+      expect(@gradebook1.courses).to eq([@calculus, @algebra])
+    end
+  end
+
+  describe '#list_all_students' do
+    it 'can return a list of courses and respective students' do
+      @gradebook1.add_course(@calculus)
+      @gradebook1.add_course(@algebra)
+
+      expect(@gradebook1.list_all_students).to eq({ Calculus: [@student1, @student2], Algebra: [@student3] })
     end
   end
 end
