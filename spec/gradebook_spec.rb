66e3463c1926cc4c322cd1542373d67031b73147
Grace Joh <graceehjoh@gmail.com>
2023-02-15 10:51:49 -0600

    test: add test for Gradebook #add_course

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe Gradebook do
   before(:each) do
     @gradebook1 = Gradebook.new('John Quintanilla')
     @gradebook2 = Gradebook.new('Dallas Williams')
-    @course1 = Course.new('Calculus', 2)
-    @course2 = Course.new('Biochemistry', 4)
+    @calculus = Course.new('Calculus', 2)
+    @lin_al = Course.new('Linear Algebra', 4)
     @student1 = Student.new({ name: 'Morgan', age: 21 })
     @student2 = Student.new({ name: 'Jordan', age: 29 })
     @student3 = Student.new({ name: "Grace", age: 31 })
@@ -22,6 +22,15 @@ RSpec.describe Gradebook do
       expect(@gradebook2).to be_a Gradebook
       expect(@gradebook2.instructor).to eq('Dallas Williams')
       expect(@gradebook2.courses).to eq([])
+    end
+  end
+
+  describe '#add_course' do
+    it 'can add a course to instructors gradebook' do
+      @gradebook1.add_course(@calculus)
+      @gradebook1.add_course(@lin_al)
+
+      expect(@gradebook1.courses).to eq([@calculus, @lin_al])
     end
   end
 end
