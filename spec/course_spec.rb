2cd0ab44bedc7c93c32911e4c3b190afc0151404
Grace Joh <graceehjoh@gmail.com>
2023-02-15 10:37:16 -0600

    test: add tests for Course #full? and @enroll

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Course do
   before(:each) do
     @student1 = Student.new({ name: 'Morgan', age: 21 })
     @student2 = Student.new({ name: 'Jordan', age: 29 })
+    @student3 = Student.new({ name: "Grace", age: 31 })
     @course1 = Course.new('Calculus', 2)
     @course2 = Course.new('Biochemistry', 4)
   end
@@ -21,6 +22,35 @@ RSpec.describe Course do
       expect(@course2.name).to eq('Biochemistry')
       expect(@course2.capacity).to eq(4)
       expect(@course2.students).to eq([])
+    end
+  end
+
+  describe '#full?' do
+    it 'can determine if course is not at capacity' do
+      expect(@course1.full?).to eq(false)
+    end
+
+    it 'can determine if course is at capacity' do
+      @course1.enroll(@student1)
+      @course1.enroll(@student2)
+
+      expect(@course1.full?).to eq(true)
+    end
+  end
+
+  describe '#enroll' do
+    it 'can add students to a course' do
+      @course1.enroll(@student1)
+      @course1.enroll(@student2)
+
+      expect(@course1.students).to eq([@student1, @student2])
+    end
+
+    it 'can alert that a course is full and will not add students' do
+      @course1.enroll(@student1)
+      @course1.enroll(@student2)
+
+      expect(@course1.enroll(@student3)).to eq('The course is full.')
     end
   end
 end
